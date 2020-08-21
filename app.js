5f8bb56295bb695f6c40d1b749e7d93711635fc7
astori <astori@umich.edu>
2020-08-21 15:33:50 -0400

    Fixes to the formatting.

---
+++ app.js
@@ -0,0 +1,10 @@
+const allHidden = document.getElementsByClassName('hidden');
+console.log(allHidden);
+const projectRelease = document.querySelector('#projrel');
+
+console.log(projectRelease)
+// console.log(projectRelease[17].innerHTML)
+
+// projectRelease[1].addEventListener('click', (event) => {
+//     console.log("this is working");
+// })
